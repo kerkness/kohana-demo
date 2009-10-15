fd3ee666d6ba47d67ab671580aefa2f5bce8aae4
Ryan Mayberry <ryankerkmayberry@gmail.com>
2009-10-15 17:17:48 -0500

    adding some more html examples

--- views/demo/html.php
+++ views/demo/html.php
@@ -23,3 +23,45 @@
 <br/>
 		
 <?php echo Html::anchor(Route::get('demo-sample')->uri(array('category'=>'books', 'id'=>5)), 'My link');  ?>
+
+<br/>
+
+<?php echo Html::email('jimmy@fallon.org'); ?>
+
+<br/>
+
+<?php echo Html::mailto('jimmy@fallon.org'); ?>
+
+<br/>
+
+<?php echo Html::style('media/css/demo/style.css'); ?>
+
+<br/>
+
+<?php echo Html::style('media/css/demo/style.css', array('media'=>'screen, projection'), TRUE); ?>
+
+<br/>
+
+<?php echo Html::script('media/js/demo/script.js'); ?>
+
+<br/>
+
+<?php echo Html::script('http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js'); ?>
+
+<br/>
+
+<?php echo Html::image('media/img/demo/icon.png'); ?>
+
+<br/>
+
+<?php echo Html::image('media/img/demo/icon.png', array('class'=>'no_border')); ?>
+
+<br/>
+
+<?php 
+ Html::$windowed_urls = TRUE;
+ echo Html::anchor('http://google.com','New Window');
+
+?>
+
+
